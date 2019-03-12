de76136b69f93c15b8d5318e66a778c96d310281
ahwillia <alex.h.willia@gmail.com>
2019-03-11 21:05:19 -0400

    update examples

--- affinewarp/_optimizers.py
+++ affinewarp/_optimizers.py
@@ -55,7 +55,6 @@ def _construct_template_optimizer(loss):
 
             # Initialize WtW with regularization term
             WtW = _diff_gramian(T, smoothness_reg_scale * K, l2_reg_scale * K)
-            DtD = WtW.copy()
 
             # Compute gramians.
             WtX = np.zeros((T, data.shape[-1]))
