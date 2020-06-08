95e91958f36fc69042754618d44f0f97f1f91f97
ahwillia <alex.h.willia@gmail.com>
2020-06-08 15:41:24 -0700

    add warning for python 2

--- affinewarp/__init__.py
+++ affinewarp/__init__.py
@@ -3,6 +3,18 @@
 __version__ = '0.1.0'
 __author__ = 'Alex Williams <ahwillia@stanford.edu>'
 
+import sys
+import warnings
+if sys.version_info[0] != 3:
+    warnings.warn(
+        "This package is supported for Python 3.+, "
+        "you are currently using {}.{}.{}".format(
+            sys.version_info[0],
+            sys.version_info[1],
+            sys.version_info[2]
+        )
+    )
+
 from .piecewisewarp import PiecewiseWarping
 from .shiftwarp import ShiftWarping
 from .spikedata import SpikeData
